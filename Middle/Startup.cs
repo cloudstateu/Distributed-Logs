53a7704b3a195a8c6805c69c25c5eb7503368b0b
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2021-06-21 19:56:41 +0200

    Initialized loggers at every layer. For PoC purposes static IP addressess are necessary.

--- Middle/Startup.cs
+++ Middle/Startup.cs
@@ -1,3 +1,4 @@
+using Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,12 +20,13 @@ namespace Middle
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Middle", Version = "v1" });
             });
+
+            services.AddJaegerLogging();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Tools/JaegerServicesConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Jaeger;
+using Jaeger.Reporters;
+using Jaeger.Samplers;
+using Jaeger.Senders.Thrift;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTracing;
+using OpenTracing.Contrib.NetCore.Configuration;
+
+namespace Common
+{
+    public static class JaegerServicesConfiguration
+    {
+        public static IServiceCollection AddJaegerLogging(this IServiceCollection services)
+        {
+            services.AddOpenTracing();
+
+            services.AddSingleton<ITracer>(_ =>
+            {
+                var serviceName = _.GetRequiredService<IWebHostEnvironment>().ApplicationName;
+                var loggerFactory = _.GetRequiredService<ILoggerFactory>();
+                var reporter = new RemoteReporter
+                    .Builder()
+                    .WithLoggerFactory(loggerFactory)
+                    .WithSender(new UdpSender("10.5.0.5", 0, 0)) // TODO Get from configuration
+                    .Build();
+
+                var tracer = new Tracer.Builder(serviceName)
+                    .WithSampler(new ConstSampler(true)) // The constant sampler reports every span.
+                    .WithReporter(reporter) // LoggingReporter prints every reported span to the logging framework.
+                    .Build();
+
+                return tracer;
+            });
+
+            services.Configure<HttpHandlerDiagnosticOptions>(_ =>
+                _.OperationNameResolver =
+                    request => $"{request.Method.Method}::{request?.RequestUri?.AbsoluteUri}");
+
+            return services;
+        }
+    }
+}
