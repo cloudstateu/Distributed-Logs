08d7cf924140258743341ab087c220e317268ab1
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2021-06-24 12:31:23 +0200

    correlation-id - reading from header

--- Middle/Startup.cs
+++ Middle/Startup.cs
@@ -1,4 +1,5 @@
 using Common;
+using Common.ActionFilters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -20,7 +21,11 @@ namespace Middle
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(_ =>
+            {
+                _.Filters.Add<OpenTracingBaggageFilter>();
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Middle", Version = "v1" });
---
+++ Tools/ActionFilters/OpenTracingBaggageFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using OpenTracing;
+using System.Linq;
+
+namespace Common.ActionFilters
+{
+    public class OpenTracingBaggageFilter : IActionFilter
+    {
+        private readonly ITracer _tracer;
+
+        public OpenTracingBaggageFilter(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            return;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var correlationId = context.HttpContext.Request.Headers["correlation-id"].FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(correlationId))
+            {
+                _tracer.ActiveSpan.SetBaggageItem("correlation-id", correlationId);
+            }
+        }
+    }
+}
